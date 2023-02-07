b25aba3b41605adc0e2427e479c92bfc2d708f86
Gijs van der Meijde <gijs.meijde@gmail.com>
2023-02-07 21:35:25 +0100

    Added protocol documentations. made fields nullable

--- KingTech.P1Reader.NuGet/P1Message.cs
+++ KingTech.P1Reader.NuGet/P1Message.cs
@@ -6,23 +6,23 @@ public record P1Message
     
     public long RawTimestamp { get; set; }//int64
 
-    public double ElectricityUsageLow { get; set; }//float64
+    public double? ElectricityUsageLow { get; set; }//float64
 
-    public double ElectricityUsageHigh { get; set; }//float64
+    public double? ElectricityUsageHigh { get; set; }//float64
 
-    public double ElectricityReturnedLow { get; set; }//float64
+    public double? ElectricityReturnedLow { get; set; }//float64
 
-    public double ElectricityReturnedHigh { get; set; }//float64
+    public double? ElectricityReturnedHigh { get; set; }//float64
 
-    public long ActiveTariff { get; set; }//int64
+    public long? ActiveTariff { get; set; }//int64
 
-    public long PowerFailuresShort { get; set; }//int64
+    public long? PowerFailuresShort { get; set; }//int64
 
-    public long PowerFailuresLong { get; set; }//int64
+    public long? PowerFailuresLong { get; set; }//int64
 
-    public double ActualElectricityDelivered { get; set; }//float64
+    public double? ActualElectricityDelivered { get; set; }//float64
 
-    public double ActualElectricityRetreived { get; set; }//float64
+    public double? ActualElectricityRetreived { get; set; }//float64
 
-    public double GasUsage { get; set; }//float64
+    public double? GasUsage { get; set; }//float64
 }
